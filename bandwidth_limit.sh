237768a1997cbb2e75454ca63f4d1904eb614547
jeonjiho <161463387+JEONJIHO817@users.noreply.github.com>
2025-07-19 21:38:52 +0900

    Update bandwidth_limit.sh

--- bandwidth_limit.sh
+++ bandwidth_limit.sh
@@ -4,39 +4,40 @@ COOR_LOG="coor_output"
 INTERFACE="eth0"
 
 # 1. coor_output에서 UL/DL 배열 파싱
-ul_line=$(grep '^UL:' "$COOR_LOG" | tail -1)
-dl_line=$(grep '^DL:' "$COOR_LOG" | tail -1)
-
-# 배열로 변환
-IFS=' ' read -r -a UL_LIST <<< "${ul_line#UL: }"
-IFS=' ' read -r -a DL_LIST <<< "${dl_line#DL: }"
-
-# 2. Block index -> IP 매핑 추출
-declare -A block_ip_map
-block_idx=0
-curr_block=""
-
+UL=()
+DL=()
 while read -r line; do
-    if [[ "$line" =~ filename:\ (/[^[:space:]]+) ]]; then
-        curr_block="${BASH_REMATCH[1]}"
+    if [[ "$line" =~ ^UL:\ (.*) ]]; then
+        IFS=' ' read -r -a UL <<< "${BASH_REMATCH[1]}"
     fi
-    if [[ "$line" =~ getLocation\.return:\ ([^[:space:]]+) ]]; then
-        ip="${BASH_REMATCH[1]}"
-        block_ip_map[$block_idx]="$ip"
-        ((block_idx++))
+    if [[ "$line" =~ ^DL:\ (.*) ]]; then
+        IFS=' ' read -r -a DL <<< "${BASH_REMATCH[1]}"
     fi
 done < "$COOR_LOG"
 
-# 3. 각 IP에 wondershaper로 대역폭 설정
-for i in "${!UL_LIST[@]}"; do
-    ul="${UL_LIST[$i]}"
-    dl="${DL_LIST[$i]}"
-    ip="${block_ip_map[$i]}"
+# 2. block number → IP 매핑 추출
+declare -A block_ip_map
+current_block=""
 
-    if [[ -n "$ip" ]]; then
-        echo "[INFO] Block $i -> IP $ip, UL=$ul DL=$dl"
+while read -r line; do
+    if [[ "$line" =~ filename:\ .*_oecobj_([0-9]+) ]]; then
+        current_block="${BASH_REMATCH[1]}"
+    fi
+    if [[ "$line" =~ getLocation.return:\ ([0-9.]+) ]]; then
+        ip="${BASH_REMATCH[1]}"
+        block_ip_map["$current_block"]="$ip"
+    fi
+done < "$COOR_LOG"
+
+# 3. UL/DL 제약 적용
+for block in "${!block_ip_map[@]}"; do
+    ip="${block_ip_map[$block]}"
+    ul="${UL[$block]}"
+    dl="${DL[$block]}"
+    if [[ -n "$ip" && -n "$ul" && -n "$dl" ]]; then
+        echo "[INFO] Block $block -> IP $ip, UL=$ul DL=$dl"
         ssh "$ip" "sudo wondershaper $INTERFACE $ul $dl"
     else
-        echo "[WARN] Block $i has no IP mapping"
+        echo "[WARN] Block $block has missing UL/DL/IP mapping"
     fi
 done
